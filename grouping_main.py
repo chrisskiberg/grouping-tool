807988c6a84b775892de9272f70cc052a657d9ae
chrisskiberg <58055220+chrisskiberg@users.noreply.github.com>
2023-11-18 21:58:55 +0100

    Savepoint

--- grouping_main.py
+++ grouping_main.py
@@ -3,6 +3,7 @@ from grouping_functions import *
 
 from math import floor, ceil
 
+
 hosts=hosts_hidden
 participants=participants_hidden
 
@@ -18,3 +19,15 @@ groups=[]
 # More hosts than one might make the group too big
 if (3<participants_per_host):
     groups.append(singular_grouping(hosts, participants))
+    # print(groups)
+    
+# Groups with one host, when there are a medium number of participants per host 
+if (2<=participants_per_host and participants_per_host<=3):
+    groups.append(medium_size_grouping(hosts, participants))
+    # print(groups)
+
+if (1<=participants_per_host and participants_per_host<2):
+    groups.append(small_size_grouping(hosts, participants))
+    # print(groups)
+
+# if (participants_per_host<=1):

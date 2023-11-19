0d604fa11efc052c5f6364cd8c72712b8c6d69b9
chrisskiberg <58055220+chrisskiberg@users.noreply.github.com>
2023-11-19 14:49:22 +0100

    Savepoint - Working parts and host generation

--- accepted_groups.py
+++ accepted_groups.py
@@ -1,10 +1,10 @@
 # accepted_groups = [[2,2], [2,3], [2,4], [2,5], [2,6], [3,4], [3,5]]
-accepted_1_host_parts = [3,4]
-accepted_2_host_parts = [2,3,4,5,6]
-accepted_3_host_parts = [4,5]
+# accepted_1_host_parts = [3,4]
+# accepted_2_host_parts = [2,3,4,5,6]
+# accepted_3_host_parts = [4,5]
 
 
-accepted_1_groups = [[1,3], [1,4]]
-accepted_2_groups = [[2,2], [2,3], [2,4], [2,5], [2,6]]
-accepted_3_groups = [[3,4], [3,5]]
+# accepted_1_groups = [[1,3], [1,4]]
+# accepted_2_groups = [[2,2], [2,3], [2,4], [2,5], [2,6]]
+# accepted_3_groups = [[3,4], [3,5]]
 # accepted_4_groups = [4,4]
--- grouping_functions.py
+++ grouping_functions.py
@@ -29,52 +29,85 @@ def medium_size_grouping(hosts=hosts_hidden, participants=participants_hidden):
     possible_combination_hosts=[]
     possible_combination_parts=[]
 
+    # combs = list(itertools.product({'a', 'b', 'c'}, repeat=10))
+    # for in range() - a, aa, aaa, aaaa, aaaaa
+    combs = list(itertools.product({130, 140, 221, 230, 240, 251, 262, 343, 352}, repeat=2))
+    # print(combs)
+    # combs_sorted=[sorted (x) for x in combs]
+    combs_sorted=[tuple (sorted (x)) for x in combs]
+    # print(combs_sorted)
+    combs = list(set(combs_sorted))
+    # print(combs)
 
-    shortest_pos_comb=0
-    longest_pos_comb=0
+    combs_HP=[]
+    for i in range(len(combs)):
+        combs_HP_i=[]
+        priority_i=0
+        host_sum_i=0
+        parts_sum_i=0
+        for j in range(len(combs[i])):
+            priority_i-=int(str(combs[i][j])[2])
+            combs_HP_i.append([int(str(combs[i][j])[0]), int(str(combs[i][j])[1])])
+        combs_HP_i.insert(0, priority_i)
+        combs_HP.append(combs_HP_i)
+        # print(priority_i, combs_HP_i)
+        print(combs_HP)
+
+
+
+
+    # accepted_1_groups = [[1,3], [1,4]]
+    # accepted_2_groups = [[2,2], [2,3], [2,4], [2,5], [2,6]]
+    # accepted_3_groups = [[3,4], [3,5]]
+
+
+    # shortest_pos_comb=0
+    # longest_pos_comb=0
     
-    # Find all the combinations of numbers that add up to the target sum
-    for i in range(1, hosts):
-        for combination in itertools.combinations_with_replacement(accepted_num_hosts_per_group, i):
-            if sum(combination) == hosts:
-                if (shortest_pos_comb==0 or len(combination)<shortest_pos_comb):
-                    shortest_pos_comb=len(combination)
-                if (longest_pos_comb==0 or longest_pos_comb<len(combination)): 
-                    longest_pos_comb=len(combination)
-                possible_combination_hosts.append(list(combination))
-    print(possible_combination_hosts)
+    # # Find all the combinations of numbers that add up to the target sum
+    # for i in range(1, hosts):
+    #     for combination in itertools.combinations_with_replacement(accepted_num_hosts_per_group, i):
+    #         if sum(combination) == hosts:
+    #             if (shortest_pos_comb==0 or len(combination)<shortest_pos_comb):
+    #                 shortest_pos_comb=len(combination)
+    #             if (longest_pos_comb==0 or longest_pos_comb<len(combination)): 
+    #                 longest_pos_comb=len(combination)
+    #             possible_combination_hosts.append(list(combination))
+    # print(possible_combination_hosts)
 
-    for i in range(shortest_pos_comb, longest_pos_comb+1):
-        for combination in itertools.combinations_with_replacement(accepted_num_parts_per_group, i):
-            if sum(combination) == participants:
-                possible_combination_parts.append(list(combination))
-                #  problem - Nå er det bare en måte å arrangere de på, men for å matche må arrangere på flere måter
-                # Kan prøve å finne riktig element, gi til noen og pope til noen elementer, hvis ingen matcher da er det ikke mulig
-                    # Men en mengde participants kan matche med flere hosts  
-    print(possible_combination_parts)    
-    print("h3i")
+    # for i in range(shortest_pos_comb, longest_pos_comb+1):
+    #     for combination in itertools.combinations_with_replacement(accepted_num_parts_per_group, i):
+    #         if sum(combination) == participants:
+    #             possible_combination_parts.append(list(combination))
+    #             #  problem - Nå er det bare en måte å arrangere de på, men for å matche må arrangere på flere måter
+    #             # Kan prøve å finne riktig element, gi til noen og pope til noen elementer, hvis ingen matcher da er det ikke mulig
+    #                 # Men en mengde participants kan matche med flere hosts  
+    # print(possible_combination_parts)    
+    # print("h3i")
 
-    for i in range(len(possible_combination_hosts)):
-        for j in range(len(possible_combination_parts)):
-            if (len(possible_combination_hosts[i])==len(possible_combination_parts[j])):
-                possible_parts=[[] for i in range(len(possible_combination_hosts[i]))]
-                for l in range(len(possible_combination_hosts[i])):
-                    if (possible_combination_hosts[i][l]==1):
-                        common = [x for x in possible_combination_parts[j] if x in accepted_1_host_parts]
-                        print(possible_combination_hosts[i][l], common)
-                    elif (possible_combination_hosts[i][l]==2):
-                        common = [x for x in possible_combination_parts[j] if x in accepted_2_host_parts]
-                        print(possible_combination_hosts[i][l], common)
-                    elif (possible_combination_hosts[i][l]==3):
-                        common = [x for x in possible_combination_parts[j] if x in accepted_3_host_parts]
-                        print(possible_combination_hosts[i][l], common)
-                    if (len(common)==0):
-                        break
-                print(possible_combination_hosts[i])
-                print(possible_combination_parts[j])
-                # print(possible_parts)
-            elif (len(possible_combination_hosts[i])<len(possible_combination_parts[j])): 
-                break
+    # for i in range(len(possible_combination_hosts)):
+    #     for j in range(len(possible_combination_parts)):
+    #         if (len(possible_combination_hosts[i])==len(possible_combination_parts[j])):
+    #             possible_parts=[[] for i in range(len(possible_combination_hosts[i]))]
+    #             for l in range(len(possible_combination_hosts[i])):
+    #                 if (possible_combination_hosts[i][l]==1):
+    #                     common = [x for x in possible_combination_parts[j] if x in accepted_1_host_parts]
+    #                     print(possible_combination_hosts[i][l], common)
+    #                 elif (possible_combination_hosts[i][l]==2):
+    #                     common = [x for x in possible_combination_parts[j] if x in accepted_2_host_parts]
+    #                     print(possible_combination_hosts[i][l], common)
+    #                 elif (possible_combination_hosts[i][l]==3):
+    #                     common = [x for x in possible_combination_parts[j] if x in accepted_3_host_parts]
+    #                     print(possible_combination_hosts[i][l], common)
+    #                 if (len(common)==0):
+    #                     break
+    #             print(possible_combination_hosts[i])
+    #             print(possible_combination_parts[j])
+    #             # print(possible_parts)
+
+    #         # Sjekke om possible combinations har alle tallene
+    #         elif (len(possible_combination_hosts[i])<len(possible_combination_parts[j])): 
+    #             break
 
 
     # all_possible_combinations=[]
